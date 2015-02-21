26737fb79fcf362a11394378aeb8e82b9b73e651
Blake Butterworth <blakebutterworth@blake.local>
2015-02-21 11:38:54 -0600

    Initial Commit

---
+++ workinghopshack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  workinghopshack
+//
+//  Created by Blake Butterworth on 2/21/15.
+//  Copyright (c) 2015 Blake Butterworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ workinghopshack/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  workinghopshack
+//
+//  Created by Blake Butterworth on 2/21/15.
+//  Copyright (c) 2015 Blake Butterworth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
